4b504446cd73db62bce22813396ab1eecfcaa218
FabrizioCatanzaro <fabriziocatanzaro1@gmail.com>
2023-01-10 22:06:09 -0300

    01

---
+++ src/layout/Layout.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import AutoToTop from '../components/AutoToTop/AutoToTop'
+import Footer from '../components/Footer/Footer'
+import Navbar from '../components/Navbar/Navbar'
+
+const Layout = (props) => {
+  return (
+    <>
+        <Navbar />
+        <div className='content-layout'>
+            {props.children}
+        </div>
+        <Footer />
+        <AutoToTop />
+    </>
+  )
+}
+
+export default Layout
