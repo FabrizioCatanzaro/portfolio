601680d6ddc87a314f124410525593cd56b77c8a
FabrizioCatanzaro <fabriziocatanzaro1@gmail.com>
2023-01-16 20:39:29 -0300

    comienzo de proyectos

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,10 +1,38 @@
 import React from 'react'
 import './Projects.css'
+import EachProject from '../../components/EachProject/EachProject'
+import listOfProjects from '../../listOfProjects'
 
 const Projects = () => {
+
+  
+  let todos = listOfProjects.map(e => e)
+  console.log(todos);
+
   return (
     <div className='projects'>
-        <p>Acá van los trabajos!</p>
+        <h2 className='titlePage'>Projects I've been working on<span className='dotColored'>.</span></h2>
+        <div className='projectsComponent'>
+
+          <div className='eachprojectCont'>
+            <div className='firstCont'>
+                <div>
+                    <p>{todos[0].name}</p>
+                </div>
+                <div className='tecnologiesProject'>
+                    <ul>
+                    {todos[0].technologies.map(dale => 
+                      <li><img src={dale.image} alt='' /></li>
+                    )}
+                    </ul>
+                </div>
+            </div>
+            <div className='secondCont'>
+              <p>{todos[0].date}</p>
+            </div>
+          </div>
+
+        </div>
     </div>
   )
 }
