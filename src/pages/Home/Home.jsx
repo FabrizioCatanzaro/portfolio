abc400e27c7a9478c527bdac001259f5a0f36e8c
FabrizioCatanzaro <fabriziocatanzaro1@gmail.com>
2023-04-06 13:09:30 -0300

    Componente Home con todas las paginas

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import AboutMe from '../AboutMe/AboutMe'
+import Projects from '../Projects/Projects'
+
+export default function Home() {
+
+    return (
+        <div>
+            <AboutMe/>
+            <Projects />
+        </div>
+    )
+}
