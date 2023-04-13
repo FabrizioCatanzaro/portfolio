50505fb43e0a9179623a7e5c0a32216dc2b83f11
FabrizioCatanzaro <fabriziocatanzaro1@gmail.com>
2023-04-12 23:15:42 -0300

    altura del boton de scrollToTop

--- src/components/ScrollToTheTop/ScrollToTheTop.jsx
+++ src/components/ScrollToTheTop/ScrollToTheTop.jsx
@@ -6,7 +6,7 @@ export default function ScrollToTheTop() {
 
     useEffect(()=>{
         const handleScrollButtonVisibility = () => {
-            window.pageYOffset > 300 ? setShowButton(true) : setShowButton(false)
+            window.pageYOffset > 250 ? setShowButton(true) : setShowButton(false)
         }
 
         window.addEventListener('scroll', handleScrollButtonVisibility)
@@ -26,7 +26,7 @@ export default function ScrollToTheTop() {
         <>
         {showButton && (
             <div className='BtnToTop' onClick={scrollUp} >
-                <svg xmlns="http://www.w3.org/2000/svg" id='btn-scrollTop' class="icon icon-tabler icon-tabler-arrow-badge-up-filled" width="44" height="44" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <svg xmlns="http://www.w3.org/2000/svg" id='btn-scrollTop' class="icon icon-tabler icon-tabler-arrow-badge-up-filled" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                     <path d="M11.375 6.22l-5 4a1 1 0 0 0 -.375 .78v6l.006 .112a1 1 0 0 0 1.619 .669l4.375 -3.501l4.375 3.5a1 1 0 0 0 1.625 -.78v-6a1 1 0 0 0 -.375 -.78l-5 -4a1 1 0 0 0 -1.25 0z" stroke-width="0" fill="currentColor"></path>
                 </svg>
