0ab79257b36fd6241f4128ae4196c0f7d32f2304
FabrizioCatanzaro <fabriziocatanzaro1@gmail.com>
2023-04-12 23:17:59 -0300

    rutas app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import Layout from './layout/Layout';
-import Projects from './pages/Projects/Projects';
 import { Route, Routes } from 'react-router-dom';
-import AboutMe from './pages/AboutMe/AboutMe';
 import Home from './pages/Home/Home';
 
 
@@ -10,9 +8,9 @@ function App() {
   return (
     <div className="App">
       <Layout>
-      <Routes>
-        <Route element={<Home />} path='/'/>
-      </Routes>
+        <Routes>
+          <Route element={<Home />} path='/'/>
+        </Routes>
       </Layout>
     </div>
   );
