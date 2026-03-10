8050e33c2f5b04cb422d4078f197c66d65ee4f48
Fabrizio Catanzaro <fabriziocatanzaro1@gmail.com>
2026-03-10 16:25:58 -0300

    fixes varios

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import Layout from './layout/Layout';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
-import { Jelly } from '@uiball/loaders'
-import { useState } from 'react';
+// import { Jelly } from '@uiball/loaders'
+//import { useState } from 'react';
 
 function App() {
 
