d20debad9f732f0ff5f514ad53975702b9113c2e
Fabrizio Catanzaro <fabriziocatanzaro1@gmail.com>
2026-03-10 16:24:39 -0300

    creacion de seccion Profile y agrego a la Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import Projects from '../Projects/Projects'
 import Intro from '../Intro/Intro'
 import Contact from '../Contact/Contact'
 import { Toaster } from 'react-hot-toast'
+import Profile from '../Profile/Profile'
 
 export default function Home() {
 
@@ -11,6 +12,7 @@ export default function Home() {
         <div>
             <Toaster />
             <Intro />
+            <Profile />
             <AboutMe/>
             <Projects />
             <Contact />
